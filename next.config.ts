c2bc98e692a22cc9d9a88317d5d5e6b2c73807e6
Gregory Row <rowgregory@gmail.com>
2025-09-03 19:59:46 -0400

    feat(parley):Bump image size limit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  api: {
+    bodyParser: {
+      sizeLimit: '4mb' // or '10mb'
+    }
+  }
+}
 
-export default nextConfig;
+export default nextConfig
