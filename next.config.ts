dfa91d0593fa9b03901a4d95ddc81de573b784e0
Gregory Row <rowgregory@gmail.com>
2025-09-03 20:04:43 -0400

    chore(config):Remove api config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  api: {
-    bodyParser: {
-      sizeLimit: '4mb' // or '10mb'
-    }
-  }
 }
 
 export default nextConfig
