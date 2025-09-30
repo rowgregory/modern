21426f548d88dfb4a8494e5aa8d6fd18928275f6
Gregory Row <rowgregory@gmail.com>
2025-09-29 20:42:24 -0400

    chore(admin-layout):Add x-user header data to admin bridge layout fetch

--- app/admin/bridge/layout.tsx
+++ app/admin/bridge/layout.tsx
@@ -2,15 +2,26 @@ import { chapterId } from '@/app/lib/constants/api/chapterId'
 import React, { FC, ReactNode } from 'react'
 import AdminBridgeClient from './admin-bridge-client'
 import { cookies } from 'next/headers'
+import { auth } from '@/app/lib/auth'
 
 const BridgeLayout: FC<{ children: ReactNode }> = async ({ children }) => {
   const cookieStore = await cookies()
+  const session = await auth() // Add this
 
   const response = await fetch(`${process.env.NEXTAUTH_URL}/api/dashboard/${chapterId}/admin/list-stats`, {
     cache: 'no-store' as RequestCache,
     headers: {
       'Content-Type': 'application/json',
-      Cookie: cookieStore.toString()
+      Cookie: cookieStore.toString(),
+      'x-user': JSON.stringify({
+        // Add this
+        id: session?.user?.id,
+        email: session?.user?.email,
+        name: session?.user?.name,
+        role: session?.user?.role,
+        isAdmin: session?.user?.isAdmin,
+        isSuperUser: session?.user?.isSuperUser
+      })
     }
   })
 
